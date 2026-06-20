0420bab3c5a62bb51715001a3a544dd2f39dde98
alshifalabs <alshifarayeen894@gmail.com>
2026-06-20 18:34:45 +0530

    updated day6 code

---
+++ DAY1/Day2/Day3/Day4/Day5/Day6/findx^n.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, n, i;
+    long long result = 1;
+
+    printf("Enter value of x: ");
+    scanf("%d", &x);
+
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        result = result * x;
+    }
+
+    printf("%d^%d = %lld", x, n, result);
+
+    return 0;
+}
